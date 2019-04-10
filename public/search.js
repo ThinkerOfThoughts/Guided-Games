3628a51a53676185a07ecb32a21a0a3353a45d56
ThinkerOfThoughts <47190876+ThinkerOfThoughts@users.noreply.github.com>
2019-04-10 16:44:53 -0400

    made functional
    while it doesn't return any usefull information, it now actually works and is simply awaiting useufll information to return

--- public/search.js
+++ public/search.js
@@ -13,21 +13,41 @@ var config = {
 firebase.initializeApp(config);
 
 //default basic search, can have username or nothing, in the event of nothing it returns everything
-function basic_search(username = 0)
+function basic_search()
 {
-    //username is what you think it is
-    return advanced_search(username);
+	//get user input from the textbox with id searchbox
+	var input = document.getElementById("searchbox").value;
+
+    //advanced search is what actually does the seraching
+
+    return advanced_search(input, 0);
 }
 
 //advance search, requires username, can use usertype
-function advanced_search(username, usertype = 0, )
+function advanced_search(input, usertype)
 {
+	var dataref = firebase.database().ref("users/");
+
+	//TODO need to return usefull information, first usefull information must be in database
+
+	//orderbychild('username') sorts results lexecographically by username, equalto(input) filters out things that don't match
+	//whatever is in input, function(snapshot){whatever} does whatever is in the curly braces when results are found, in this case 			//alert("user found: " + snapshot.child("email").val()) prints a users email address, this is simply a placeholder 
+	dataref.orderByChild('username').equalTo(input).on("child_added", function(snapshot) {
+	 alert("users found: " + snapshot.child("email").val());
+	});
     //usertype is an integer, 0 for all users, 1 for mentees, 2 for mentors
-    if(username === 0)
-    {
-          console.log(snapshot.val());
-    }
-    
-    
-    
+	if(usertype === 0)
+	{
+		//alert(input); //added for testing          
+	}
+	else if(usertype === 1)
+	{
+
+	}
+	else if(usertype === 3)
+	{
+
+	}
 }
+
+//document.querySelector('#Searchtext').addEventListener('Search', basic_search);
