8681e5fc2d0330727794c6a6a0bdf5bb7d0e70d6
thinkerofthoughts <RVRgeek@gmail.com>
2019-04-17 16:47:55 -0400

    Cleaned, made search function
    Removed two unnesiary files (oldlayout.css, faq.html);
    made search function more functional, now returns values that contain
    the searched for term, redirects to html page containing results. Next
    step is to make results clickable to send user to profile page

--- public/search.js
+++ public/search.js
@@ -27,27 +27,16 @@ function basic_search()
 function advanced_search(input, usertype)
 {
 	var dataref = firebase.database().ref("users/");
+	var result_found = false;	//boolean to hold if a search result is found
 
-	//TODO need to return usefull information, first usefull information must be in database
+	var new_window = window.open("search_results_test.html");
 
-	//orderbychild('username') sorts results lexecographically by username, equalto(input) filters out things that don't match
-	//whatever is in input, function(snapshot){whatever} does whatever is in the curly braces when results are found, in this case 			//alert("user found: " + snapshot.child("email").val()) prints a users email address, this is simply a placeholder 
-	dataref.orderByChild('username').equalTo(input).on("child_added", function(snapshot) {
-	 alert("users found: " + snapshot.child("email").val());
+	//orderbychild('username') sorts results lexecographically by username, startAt(input).endAt(input+"\uf8ff") filters out things that 		//don't match whatever is in input, function(snapshot){whatever} does whatever is in the curly braces when results are found, in this 		//case; alert("user found: " + snapshot.child("email").val()) prints a users email address, this is simply a placeholder 
+
+	dataref.orderByChild('username').startAt(input).endAt(input+"\uf8ff").on("child_added", function(snapshot) {
+		new_window.document.getElementById('results').innerHTML += '<div style="background-color:#87DCFF; text-align:left; 			vertical-align: middle; padding:20px 47px; width:420px; margin:0 auto;" align="center">' + snapshot.child("username").val() + 			" : " + snapshot.child("email").val() + '</div> <br>  <br> ';
 	});
-    //usertype is an integer, 0 for all users, 1 for mentees, 2 for mentors
-	if(usertype === 0)
-	{
-		//alert(input); //added for testing          
-	}
-	else if(usertype === 1)
-	{
-
-	}
-	else if(usertype === 3)
-	{
-
-	}
+	//TODO make reuslts clickable, clicking takes you to profile page
+	//TODO make searchable by criteria other than just username
+	//TODO return more usefull information than just username/email address
 }
-
-//document.querySelector('#Searchtext').addEventListener('Search', basic_search);
