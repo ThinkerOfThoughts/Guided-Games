2732a55e2e569cc2be3932c328ef71534c7a7dd6
Luiz Hosfeld <Lh15s@my.fsu.edu>
2019-04-17 21:12:47 -0400

    Added username verification (Luiz & Hannah)
    Declined user creation if username is already taken.

--- public/signup.js
+++ public/signup.js
@@ -20,55 +20,63 @@ function sign_up(event)
 	const password2 = form.password2.value;
 	const username = form.username.value;
 	
-	if(password != password2)
-	{
-		alert("Passwords do not match!");
-	}
-	else if(username == "")
-	{
-		alert("Username is blank!");
-	}
-	else if(password == "")
-	{
-		alert("Password is blank!");
-	}
-	else if(email == "")
-	{
-		alert("Email is blank!");
-	}
-	else
-	{
-		firebase.auth().createUserWithEmailAndPassword(email, password).then(function(response){
-			let users = firebase.database().ref('users');
-			const newKey = users.push().key;
+	var dataref = firebase.database().ref("users/");
+	
+	dataref.orderByChild('username').equalTo(username).once("value", function(snapshot){
+		if(!(snapshot.exists()))
+		{
 			
-			let newData = {
-				username: username,
-				email: email
-			};
-			
-			firebase.database().ref('users/' + newKey).update(newData, function(error){
-				if(error){
-					alert(error);
-				}
-				else
-				{
-					alert('User has been created!');
-					window.location.href = './home.html';
-				}
-			});
-			
-			
-			
-		}).catch(function(error){
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			alert(errorCode);
-			alert(errorMessage);
-		});
-		//const url = new URL('http://localhost:3000/register');
-		//let params = {username: username, email: email};
-	}
+			if(password != password2)
+			{
+				alert("Passwords do not match!");
+			}
+			else if(username == "")
+			{
+				alert("Username is blank!");
+			}
+			else if(password == "")
+			{
+				alert("Password is blank!");
+			}
+			else if(email == "")
+			{
+				alert("Email is blank!");
+			}
+			else
+			{
+				firebase.auth().createUserWithEmailAndPassword(email, password).then(function(response){
+					let users = firebase.database().ref('users');
+					const newKey = users.push().key;
+					
+					let newData = {
+						username: username,
+						email: email
+					};
+					
+					firebase.database().ref('users/' + newKey).update(newData, function(error){
+						if(error){
+							alert(error);
+						}
+						else
+						{
+							alert('User has been created!');
+							window.location.href = './home.html';
+						}
+					});
+					
+					
+					
+				}).catch(function(error){
+					var errorCode = error.code;
+					var errorMessage = error.message;
+					alert(errorCode);
+					alert(errorMessage);
+				});
+			}
+		}
+		else
+			alert("Username already exists!");
+	});
 	
 }
 
