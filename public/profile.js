22eb95b8e761640b072f22e043178bbfc90f8ff5
thinkerofthoughts <RVRgeek@gmail.com>
2019-04-21 15:53:22 -0400

    renamed profile.html
    renamed profile.html, attempted to get search.js working, failed...

--- public/profile.js
+++ public/profile.js
@@ -15,7 +15,14 @@ firebase.initializeApp(config);
 //retrieves the data from the api using the provided username
 function profile(username)
 {
-	var new_window = window.open("profile.html");
+	//remove when sending data is fixed
+	username = "test1";
+
+	var result_window = window.open("user_profile.html");
+
+	//alert(username);
+
 	//TODO figure out how to transfer username into this function
 	//TODO add code to put data from api into profile.html
+	result_window.windows.getElementById('user_profile').innerHTML = '<div align="center"; style="background-color:#87DCFF; text-align:left; vertical-align: middle; padding:20px 47px; width:420px; margin:0 auto;" align="center"> Profile for </div>';
 }
--- public/search.js
+++ public/search.js
@@ -12,6 +12,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
 //default basic search, can have username or nothing, in the event of nothing it returns everything
 function basic_search()
 {
@@ -27,16 +28,26 @@ function basic_search()
 function advanced_search(input, usertype)
 {
 	var dataref = firebase.database().ref("users/");
-	var result_found = false;	//boolean to hold if a search result is found
 
 	var new_window = window.open("search_results_test.html");
+
+
+	var usernames = [];	//will hold usernames returned with each search result, seemed simpler than trying to figure out how to
+				//backtrack
+
+	var counter = 0;	//used to find stuff in usernames
 
 	//orderbychild('username') sorts results lexecographically by username, startAt(input).endAt(input+"\uf8ff") filters out things that 		//don't match whatever is in input, function(snapshot){whatever} does whatever is in the curly braces when results are found, in this 		//case; alert("user found: " + snapshot.child("email").val()) prints a users email address, this is simply a placeholder 
 
 	dataref.orderByChild('username').startAt(input).endAt(input+"\uf8ff").on("child_added", function(snapshot) {
-		new_window.document.getElementById('results').innerHTML += '<div align="center"><button id="result_button" onclick="profile()"; style="background-color:#87DCFF; text-align:left; vertical-align: middle; padding:20px 47px; width:420px; margin:0 auto;" align="center">' + snapshot.child("username").val() + " : " + snapshot.child("email").val() + '</button></div> <br>  <br> ';
-	});
+		usernames.push(snapshot.child("username").val());
+		new_window.document.getElementById('results').innerHTML += '<div align="center"><button id="result_button' + counter + '"  onclick="profile(2)" style="background-color:#87DCFF; text-align:left; vertical-align: middle; padding:20px 47px; width:420px; margin:0 auto;" align="center">' + usernames[counter] + " : " + snapshot.child("email").val() + '</button></div> <br>  <br> ';
+	//the profile(2) is just so I can work on profile.js, replace with whatever will transfer data correctly when that is found
 
+		counter++;	//having this at the end avoids an off by one error
+	// new_window.document.getElementById("result_button" + counter).addEventListener("click", alert(counter));//"click", profile(username);
+
+	});
 	//TODO make searchable by criteria other than just username
 	//TODO return more usefull information than just username/email address
 }
