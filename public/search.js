73abf8e321f9ab135bea1a1f7d99419daeb9fee8
thinkerofthoughts <RVRgeek@gmail.com>
2019-04-24 16:23:34 -0400

    Advanced search now functional
    advanced search is now functional, advanced_search.html now has all
    currently searchable fields

--- public/search.js
+++ public/search.js
@@ -24,12 +24,7 @@ function basic_search()
 //advance search, requires username, can use usertype
 function advanced_search(searchtype)
 {
-	//searchtype means is being used to distinguis between basic and advanced searches
-	if(searchtype === 1)
-	{
-		//get user input from the textbox with id searchbox
-		var input = document.getElementById("searchbox").value;
-
+		var input = "";
 		let dataref = firebase.database().ref("users/");
 
 		let new_window = window.open("search_results_test.html");
@@ -40,25 +35,32 @@ function advanced_search(searchtype)
 
 		var counter = 0;	//used to find stuff in usernames
 
+	//searchtype means is being used to distinguis between basic and advanced searches
+	if(searchtype === 1)
+	{
+		//get user input from the textbox with id searchbox
+		if(document.getElementById("matches").value != null)
+		{
+			input = document.getElementById("advanced_searchbox").value;
+		}
+
 	//orderbychild('username') sorts results lexecographically by username, startAt(input).endAt(input+"\uf8ff") filters out things that 		//don't match whatever is in input, function(snapshot){whatever} does whatever is in the curly braces when results are found, in this 		//case; alert("user found: " + snapshot.child("email").val()) prints a users email address, this is simply a placeholder 
 
-		dataref.orderByChild('username').startAt(input).endAt(input+"\uf8ff").on("child_added", function(snapshot) {
+		dataref.orderByChild('username').startAt(input).endAt(input+"\uf8ff").on("child_added", function(snapshot)
+		{
 			usernames.push(snapshot.child("username").val());
 			new_window.document.getElementById('results').innerHTML += '<div align="center"><button id="result_button' + counter + '"  onclick="profile(\'' + usernames[counter] + '\')" style="background-color:#87DCFF; text-align:left; vertical-align: middle; padding:20px 47px; width:420px; margin:0 auto;" align="center">' + usernames[counter] + " : " + snapshot.child("email").val() + '</button></div> <br>  <br> ';
 			counter++;	//having this at the end avoids an off by one error
 		});
-		//TODO make searchable by criteria other than just username
-		//TODO return more usefull information than just username/email address
-		new_window.document.close();
+
 	}
-/* 	else if(searchtype === 2)
+ 	else if(searchtype === 2)
 	{
 		//handling data collection
 		//default minimum values
-		var input = null;
 		var matches = 0;
 		var pvp_kills = 0;
-		var pvp_deaths = 0;
+		//var pvp_deaths = 0; //not going to be searchable
 		var overall_kd = 0;
 		var overall_kda_game = 0;
 		var most_kills_game = 0;
@@ -67,20 +69,76 @@ function advanced_search(searchtype)
 		var highest_level = 0;
 		var power_level = 0;
 		var raids_cleared = 0;
-		var fastest_time = 0;
-		if(document.getElementById("advanced_searchbox").value != null)
-		{		
-			input = document.getElementById("advanced_searchbox").value;
-		}
-		
-		if(document.getElementById("raids").value != null)
+		//var fastest_time = "0";	//not currently searchable
+		if(document.getElementById("matches").value != null)
 		{
-			raids_cleared = document.getElementById("raids").value;
+		input = document.getElementById("advanced_searchbox").value;
 		}
-		
-		dataref.orderByChild('username').startAt(input).endAt(username+"\uf8ff").on("child_added", function(snapshot) {
+		if(document.getElementById("matches").value != null)
+		{
+			matches = document.getElementById("matches").value;
+		}
 
-			usernames.push(snapshot.child("username").val());
+		if(document.getElementById("pvp_kills").value != null)
+		{
+			pvp_kills = document.getElementById("pvp_kills").value;
+		}
+
+		if(document.getElementById("overall_kd").value != null)
+		{
+			overall_kd = document.getElementById("overall_kd").value;
+		}
+
+		if(document.getElementById("overall_kda_game").value != null)
+		{
+			overall_kda_game = document.getElementById("overall_kda_game").value;
+		}
+
+		if(document.getElementById("most_kills_game").value != null)
+		{
+			most_kills_game = document.getElementById("most_kills_game").value;
+		}
+
+		if(document.getElementById("longest_streak").value != null)
+		{
+			longest_streak = document.getElementById("longest_streak").value;
+		}
+
+		if(document.getElementById("win_loss").value != null)
+		{
+			win_loss = document.getElementById("win_loss").value;
+		}
+
+		if(document.getElementById("highest_level").value != null)
+		{
+			highest_level = document.getElementById("highest_level").value;
+		}
+
+		if(document.getElementById("power_level").value != null)
+		{
+			power_level = document.getElementById("power_level").value;
+		}
+
+		if(document.getElementById("raids_cleared").value != null)
+		{
+			raids_cleared = document.getElementById("raids_cleared").value;
+		}
+
+//		if(document.getElementById("fastest_time").value != null)
+//		{
+//			fastest_time = document.getElementById("fastest_time").value;
+//		}
+
+		dataref.orderByChild('username').startAt(input).endAt(input+"\uf8ff").on("child_added", function(snapshot)
+		{
+			if(snapshot.child("matches").val() >= matches && snapshot.child("pvp_kills").val() >= pvp_kills && 				snapshot.child("overall_kd").val() >= overall_kd && snapshot.child("overall_kda_game").val() >= overall_kda_game && 			snapshot.child("most_kills_game").val() >= most_kills_game && snapshot.child("longest_streak").val() >= longest_streak 				&& snapshot.child("win_loss").val() >= win_loss && snapshot.child("highest_level").val() >= highest_level && 				snapshot.child("power_level").val() >= power_level && snapshot.child("raids_cleared").val() >= raids_cleared)// && 			snapshot.child("fastest_time").val() <= fastest_time)
+			{
+				usernames.push(snapshot.child("username").val());
 			new_window.document.getElementById('results').innerHTML += '<div align="center"><button id="result_button' + counter + '"  onclick="profile(\'' + usernames[counter] + '\')" style="background-color:#87DCFF; text-align:left; vertical-align: middle; padding:20px 47px; width:420px; margin:0 auto;" align="center">' + usernames[counter] + " : " + snapshot.child("email").val() + '</button></div> <br>  <br> ';
-*/	
-	};
+			counter++;	//having this at the end avoids an off by one error
+
+			}
+		});
+	}
+	new_window.document.close();
+}
