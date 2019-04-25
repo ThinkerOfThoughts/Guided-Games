9c13a5823e36d0f41b2d1ab2a8dc03408417ad0c
heaven <hfm16@my.fsu.edu>
2019-04-25 00:25:08 -0400

    made some edits to beautify

---
+++ public/arrange.js
@@ -0,0 +1,10 @@
+var socket = io.connect('http://localhost:3000');
+var config = {
+    apiKey: "AIzaSyD1lj9odK753YVBGQECer5DplzZ6AYiNM8",
+    authDomain: "guided-games.firebaseapp.com",
+    databaseURL: "https://guided-games.firebaseio.com",
+    projectId: "guided-games",
+    storageBucket: "guided-games.appspot.com",
+    messagingSenderId: "887475943898"
+  };
+firebase.initializeApp(config);
